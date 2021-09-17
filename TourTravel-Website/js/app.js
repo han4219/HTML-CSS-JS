33f0dad98d43a97cad0ed5763aab679efcf50245
An <han157589@gmail.com>
2021-09-17 08:49:35 +0700

    update

--- TourTravel-Website/js/app.js
+++ TourTravel-Website/js/app.js
@@ -50,3 +50,47 @@ videoBtns.forEach((btn) => {
     videoSrc.src = src;
   });
 });
+
+// swiper
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".brand-slider", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
